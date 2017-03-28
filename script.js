e314d2ce8b8bacfa1328de68cc1d79fa79f32f16
Emmanuel Martinez <emmanuel.m.student@lyonscommunityschool.org>
2017-03-28 14:18:42 +0000

    create password guesser

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 $(document).ready(function(){
-    var password = "5";
-    var input = $("#input").val();
+
 $("#enter").click(function() {
+    var password = 6 ;
+    var input = $("#input").val();
 
-
-if (password === $("#input").val())
+if (password === input)
 {
     
     $("#apples").text("congragulastions you played yourself");
     alert('suhhdude');
     
 }
-else{
-   if(input > password){
+
+else if(input > password){
        alert("too high");
    }
-}
 
+else if(input < password){
+        alert("too low");
+    }
+    
 
-var name = $("#input").val();
 
 });
 });
